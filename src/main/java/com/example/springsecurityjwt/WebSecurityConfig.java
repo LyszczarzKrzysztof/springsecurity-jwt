3260b86b227c25f64bbec3355f7ef044a82f8b7b
LyszczarzKrzysztof <krzysiek.lyszczarz@o2.pl>
2020-06-14 15:57:12 +0200

    Access by role admin to test3, authentication to test2.

---
+++ src/main/java/com/example/springsecurityjwt/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.example.springsecurityjwt;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    @Bean
+//    public PasswordEncoder getPasswordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication().withUser(User.builder()
+//                .username("test")
+//                .password(getPasswordEncoder().encode("test"))
+//                .roles("ADMIN"));
+//    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/test2")
+                .authenticated()
+                .antMatchers("/test3")
+                .hasAnyRole("ADMIN")
+                .and()
+                .addFilter(new JwtFilter(authenticationManager()))
+                .formLogin()
+                .permitAll();
+    }
+}
